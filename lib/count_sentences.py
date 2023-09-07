8ef21dd36039dd4878e57d453279672ce7bc0ce1
Maderu-CM <clive.moyia@student.moringaschool.com>
2023-09-08 02:36:14 +0300

    Assignment Done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+    def __init__(self, value):
+        if isinstance(value, str):
+            self.value = value
+        else:
+            raise ValueError("Value must be a string")
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+       
+        sentences = re.split(r'[.!?]', self.value)
+        
+        return len([sentence for sentence in sentences if sentence.strip()])
+
+# test
+string = MyString("This is a string! It has three sentences. Right?")
+print("Is a sentence:", string.is_sentence())
+print("Is a question:", string.is_question())
+print("Is an exclamation:", string.is_exclamation())
+print("Count of sentences:", string.count_sentences())
+
